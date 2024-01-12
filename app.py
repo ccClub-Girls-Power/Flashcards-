058d49511136705aa23b1fa96303468b4c094cf2
st418s21 <131063126+st418s21@users.noreply.github.com>
2024-01-12 15:29:32 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -704,9 +704,7 @@ def create_flex_dictionary_card(pos_list, chinese_list, current_time, us_pron_ur
 
     flex_contents.extend(fixed_contents)
 
-    return FlexSendMessage(
-        alt_text=word,
-        contents={
+    return {
             "type": "bubble",
             "body": {
                 "type": "box",
@@ -719,8 +717,7 @@ def create_flex_dictionary_card(pos_list, chinese_list, current_time, us_pron_ur
                 ],
             },
             "styles": {"footer": {"separator": True}},
-        },
-    )
+        }
 
 # 函數：查看更多卡片
 def generate_see_more_bubble():
@@ -2843,14 +2840,35 @@ flashcard/flash card"""
                     columns_list.append(name)
                     data_lists.append(data_list)
 
-                flex_message = create_flex_dictionary_card(data_lists[2][0], data_lists[3][0], data_lists[0][0],
-                                                           data_lists[5][0], data_lists[6][0], data_lists[1][0])
+                flex_messages = [
+                    create_flex_dictionary_card(pos, chinese, current_time, us_pron, uk_pron, word)
+                    for pos, chinese, current_time, us_pron, uk_pron, word
+                    in zip(data_lists[2], data_lists[3], data_lists[0], data_lists[5], data_lists[6], data_lists[1])
+                ]
+                user_card_index[user_id] = 0
 
+                if len(flex_messages) <= 10:
+                    # 少於等於 10 條 Bubble Messages，使用 Carousel Flex Message
+                    carousel_flex_message = FlexSendMessage(
+                        alt_text="Carousel Flex Message",
+                        contents={
+                            "type": "carousel",
+                            "contents": flex_messages
+                        }
+                    )
+                else:
+                    # 多於 10 條 Bubble Messages，使用 Carousel Flex Message 加上 See More 按鈕（因為carousel最多只能顯示10個Bubble Messages)
+                    carousel_flex_message = FlexSendMessage(
+                        alt_text="Carousel Flex Message",
+                        contents={
+                            "type": "carousel",
+                            "contents": flex_messages[:9] + [generate_see_more_bubble()]
+                        }
+                    )
+                line_bot_api.reply_message(event.reply_token, carousel_flex_message)
+                user_flex_messages[user_id] = flex_messages
 
-                flex_reply_message = FlexSendMessage(alt_text="字典卡", contents=flex_message)
-                line_bot_api.reply_message(event.reply_token, flex_reply_message)
-
-
+            
 
 
     # 選擇學習模式__複習模式
